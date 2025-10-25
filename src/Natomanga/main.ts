1554c65795bf4bc37f19afa2ae9a3f8160a1f91f
Saw_6 <virgile@pc-235.home>
2025-10-25 11:28:15 +0200

    Natomanga ext

--- src/Natomanga/main.ts
+++ src/Natomanga/main.ts
@@ -450,7 +450,7 @@ export class NatomangaExtension implements NatomangaImplementation {
     }
 
     private async fetchCheerio(request: Request): Promise<CheerioAPI> {
-        const [response, data] = await Application.scheduleRequest(request);
+        const [, data] = await Application.scheduleRequest(request);
         const htmlStr = Application.arrayBufferToUTF8String(data);
         const dom = htmlparser2.parseDocument(htmlStr);
         return cheerio.load(dom);
