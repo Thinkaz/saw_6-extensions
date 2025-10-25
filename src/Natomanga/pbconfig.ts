ac608cedfe4e30275eb78477e353b5c6be9b78de
Saw_6 <virgile@pc-235.home>
2025-10-25 22:04:26 +0200

    Natomanga : me vs cloudflare - who will win? - v2

--- src/Natomanga/pbconfig.ts
+++ src/Natomanga/pbconfig.ts
@@ -17,6 +17,7 @@ export default {
         SourceIntents.DISCOVER_SECIONS_PROVIDING |
         SourceIntents.SEARCH_RESULTS_PROVIDING |
         SourceIntents.CHAPTER_PROVIDING |
+        SourceIntents.CLOUDFLARE_BYPASS_REQUIRED |
         SourceIntents.CLOUDFLARE_BYPASS_PROVIDING,
     badges: [],
     developers: [
