2d931cf99909c07c299663e848c9d38c05eda5b7
Saw_6 <virgile@pc-235.home>
2025-11-05 19:28:04 +0100

    fix(roliascan): reader adblocker

--- src/Roliascan/main.ts
+++ src/Roliascan/main.ts
@@ -576,10 +576,16 @@ export class RoliascanExtension implements RoliascanImplementation {
             imgUrl = imgUrl.trim();
 
             if (
-                imgUrl.includes("roliascan.com/wp-content/uploads/2024/07/warning-1.png") ||
-                imgUrl.includes("roliascan.com/wp-content/uploads/2025/09/end-chapter.jpg")
+                imgUrl.includes(
+                    "roliascan.com/wp-content/uploads/2024/07/warning-1.png",
+                ) ||
+                imgUrl.includes(
+                    "roliascan.com/wp-content/uploads/2025/09/end-chapter.jpg",
+                )
             ) {
-                console.log(`[Roliascan] Skipped unwanted image: ${imgUrl.substring(0, 50)}...`);
+                console.log(
+                    `[Roliascan] Skipped unwanted image: ${imgUrl.substring(0, 50)}...`,
+                );
                 return;
             }
 
